8c5ede7fc1d79afe52b0d81aaeb9ead67ebbf092
egoRockU <lagutan.gerwin.bscs2021@gmail.com>
2024-07-22 18:39:20 +0800

    current progress

---
+++ components/EmojiSticker.js
@@ -0,0 +1,18 @@
+import { View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+
+export default function EmojiSticker({ imageSize, stickerSource }) {
+  const scaleImage = useSharedValue(imageSize);
+
+  return (
+    <View style={{ top: -350 }}>
+      <Animated.Image
+        source={stickerSource}
+        resizeMode="contain"
+        style={{ width: imageSize, height: imageSize }}
+      />
+    </View>
+  );
+};
+
